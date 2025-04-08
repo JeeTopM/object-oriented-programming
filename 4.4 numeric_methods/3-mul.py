ab877d52db364b4b54ed8848ed4adf4ebc186d67
Лев Романович Панас <jieba2005@list.ru>
2025-04-08 18:24:43 +0300

    add 6 tasks

---
+++ 4.4 numeric_methods/3-mul.py
@@ -0,0 +1,10 @@
+# Sample Output: Hello!Hello!Hello!
+
+class Hello:
+    def __init__(self, say):
+        self.say = say
+
+    # Ваш код
+
+lang = Hello('Hello!')
+print(lang * 3)  # Hello!Hello!Hello!
---
+++ 4.4 numeric_methods/6-abs.py
@@ -0,0 +1,15 @@
+# 80 -1.0 10 100 -30 10
+
+class Number:
+    # Объявите здесь все необходимые методы для операций
+    pass
+
+# код ниже не меняйте ради вселенной
+num1 = Number(-10)
+result1 = num1 + 90
+result2 = num1 / 10
+result3 = num1 * -1
+result4 = num1 ** 2
+result5 = num1 - 20
+result6 = abs(num1)
+print(result1, result2, result3, result4, result5, result6)
